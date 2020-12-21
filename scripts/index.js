e13da1bb03b400276458931d8a7cc401f2b56b9b
Emil Bakhshiev <emilbahshiev@gmail.com>
2020-12-22 00:58:45 +0300

    feat: сделать валидацию форм часть 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,30 @@
 const editButton = document.querySelector('.profile__button-edit');
 const popupProfileButtonClose = document.querySelector('#close-edit-popup');
 const editPopup = document.querySelector('#edit-popup');
-const inputName = document.querySelector('#name');
-const inputAboutMe = document.querySelector('#about-me');
 const profileName = document.querySelector('.profile__name');
 const profileAboutMe = document.querySelector('.profile__description');
-const saveButton = document.querySelector('.popup__save-button');
+
 const addButton = document.querySelector('.profile__button-add');
 const addCardPopup = document.querySelector('#add-card-popup');
 const galeryCardContainer = document.querySelector('.galery');
 const templateCard = document.querySelector('template');
 const closePopupAddCard = document.querySelector('#close-add-popup');
-const inputPlaceName = document.querySelector('#place-name');
-const inputPlaceImage = document.querySelector('#image-link');
 const createButton = document.querySelector('.popup__create-button');
 const imagePopup = document.querySelector('#image-popup');
 const closeButtonimagePopup = document.querySelector('#close-image-popup');
 const fullSizeImage = document.querySelector('.popup__image');
 const imageCaption = document.querySelector('.popup__caption');
+
+//Add card form
+const formAddCard = document.querySelector('#add-card-form');
+const inputPlaceName = formAddCard.querySelector('#place-name');
+const inputPlaceImage = formAddCard.querySelector('#image-link');
+
+//Edit form
+const formEditProfile = document.querySelector('#edit-profile-form');
+const inputName = formEditProfile.querySelector('#name');
+const inputAboutMe = formEditProfile.querySelector('#about-me');
+
 
 //Добавление карточек из шаблона
 function renderCards() {
@@ -79,20 +86,74 @@ function onLikeButton(event) {
 }
 
 //Работа с формой редактирования профиля
-const formEditProfile = document.querySelector('#edit-profile-form');
 
 function handleFormSubmitProfile(evt) {
-    evt.preventDefault();
     profileName.textContent = inputName.value;
     profileAboutMe.textContent = inputAboutMe.value;
     closePopup(editPopup);
 }
 formEditProfile.addEventListener('submit', handleFormSubmitProfile);
 
+//РАБОТА С ФОРМАМИ
+
+function showError(form, input) { //показ ошибки
+    const error = form.querySelector(`#${input.id}-error`);
+    error.textContent = input.validationMessage;
+    input.classList.add('popup__input_state_invalid');
+}
+
+function hideError(form, input) {
+    const error = form.querySelector(`#${input.id}-error`);
+    error.textContent = "";
+    input.classList.remove('popup__input_state_invalid');
+}
+
+function checkInputValidity(form, input) { //проверяет валидность
+    if (!input.validity.valid) {
+        showError(form, input);
+    } else {
+        hideError(form, input);
+    }
+}
+
+function setButtonState(button, isActive) { //Выбирает состояние кнопки
+    if (isActive) {
+        button.classList.remove('.popup__button_invalid');
+        button.disabled = false;
+    } else {
+        button.classList.add('.popup__button_invalid');
+        button.disabled = true;
+    }
+}
+
+function setEventListener(form) {
+    const inputList = form.querySelectorAll('.popup__input');
+    const submitButton = form.querySelector('.popup__button');
+    inputList.forEach(input => {
+        input.addEventListener('input', (evt) => {
+            checkInputValidity(form, input);
+            setButtonState(submitButton, form.checkValidity());
+        })
+    })
+}
+
+function enableValidation() {
+    const forms = document.querySelectorAll('.popup__container');
+    forms.forEach(form => {
+        setEventListener(form);
+        form.addEventListener('submit', (evt) => {
+            evt.preventDefault()
+        });
+        const submitButton = form.querySelector('.popup__button');
+        setButtonState(submitButton, form.checkValidity())
+    });
+
+}
+
+enableValidation();
+
 // Добавление новых карточек
-const formAddCard = document.querySelector('#add-card-form');
-function handlePlaceSubmitAddCard(evt) {
-    evt.preventDefault();
+function handlePlaceSubmitAddCard() {
     const newCardTitle = inputPlaceName.value;
     const newCardImage = inputPlaceImage.value;
     const newCardItem = composeCard({ name: newCardTitle, link: newCardImage });
